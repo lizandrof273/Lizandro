6ba8a3a806e9b74d405f9c55915c816f5c3c7e1c
lizandrof273 <lizandrof273@gmail.com>
2020-08-16 01:22:47 -0700

    Updates

---
+++ precache-manifest.a728977bc865960edbb3e4c296afcd65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1ca5668ffbcdd75edafc52182bee1d",
+    "url": "/Lizandro/index.html"
+  },
+  {
+    "revision": "2f926d48750db8025918",
+    "url": "/Lizandro/static/css/main.323ac80f.chunk.css"
+  },
+  {
+    "revision": "4c460d8f8fa73db6710d",
+    "url": "/Lizandro/static/js/2.88b8f7cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lizandro/static/js/2.88b8f7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f926d48750db8025918",
+    "url": "/Lizandro/static/js/main.baa755ea.chunk.js"
+  },
+  {
+    "revision": "339abdeda1dce99b059d",
+    "url": "/Lizandro/static/js/runtime-main.4ee4791c.js"
+  }
+]);
