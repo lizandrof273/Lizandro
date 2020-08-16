a78cc8cbb88dd20ae6dd05714408e7cb2bc0e30e
lizandrof273 <lizandrof273@gmail.com>
2020-08-15 23:48:04 -0700

    got hello world to display!

--- lizando-web/src/index.js
+++ lizando-web/src/index.js
@@ -1,22 +1,12 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import Library from './Library'
+//import 'bootstrap/dist/css/bootstrap.css';
 
-let bookList = [
-  {"title": "Hunger", "author": "Roxane Gay", "pages": "320"},
-  {"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": "260"},
-  {"title": "White Teeth", "author": "Zadie Smith", "pages": "480"},
-  {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": "304"}
-]
-
-render(
-  <Library books={bookList}/>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
