c4e5a4fa565bc2a67b6437e3c32322fad4887cd6
lizandrof273 <lizandrof273@gmail.com>
2020-08-15 22:40:45 -0700

    added css and new format to react

--- lizando-web/src/index.js
+++ lizando-web/src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import Library from './Library'
+
+let bookList = [
+  {"title": "Hunger", "author": "Roxane Gay", "pages": "320"},
+  {"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": "260"},
+  {"title": "White Teeth", "author": "Zadie Smith", "pages": "480"},
+  {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": "304"}
+]
+
+render(
+  <Library books={bookList}/>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
