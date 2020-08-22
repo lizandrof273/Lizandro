a215b1e1abecb558a0d166881cffa2a225ca8738
lizandrof273 <lizandrof273@gmail.com>
2020-08-22 01:06:32 -0700

    added button that takes user to mail with email completed

--- lizando-web/src/Contact.js
+++ lizando-web/src/Contact.js
@@ -4,8 +4,10 @@ import './myStyles.css'
 export const Contact = () => 
 <div className={`jumbotron container`}>
 <h2>External sites</h2>
-  <p>Learn more about <b>me</b>.</p>
+  <p>Learn more about <b>me</b> on other sites.</p>
+  <a class="btn" href={"mailto:" + 'lizandrof273@gmail'}>Contact me</a>
   <a class="btn" href="https://www.linkedin.com/in/lizandrofranco/" target="_blank">Linkedln</a>
   <a class="btn" href="https://github.com/lizandrof273" target="_blank">Github</a>
+  
 
  </div>
--- lizando-web/src/Nav.js
+++ lizando-web/src/Nav.js
@@ -4,8 +4,9 @@ import './myStyles.css'
 export const Nav = () => 
 <div className={`nav container`}id ="down">
         <ul>
-          <li>Projects</li>
-          <li>Resume</li>
-          <li>Skill Set</li>
+          <a href="#" target="_blank"><li>Projects</li></a>
+          <a href="https://drive.google.com/file/d/1V92OZer_RUE9qPndAAiJvlZgdFxUak_e/view?usp=sharing" target="_blank"><li>Resume</li></a>
+          <a href="#" target="_blank"><li>Skill Set</li></a>
+        
         </ul> 
  </div>
