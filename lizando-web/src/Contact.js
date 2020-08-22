9c87ec22d0be6b94483d3e40605e2b53e3ac3064
lizandrof273 <lizandrof273@gmail.com>
2020-08-22 00:40:49 -0700

    goes to new page

--- lizando-web/src/Contact.js
+++ lizando-web/src/Contact.js
@@ -5,7 +5,7 @@ export const Contact = () =>
 <div className={`jumbotron container`}>
 <h2>External sites</h2>
   <p>Learn more about <b>me</b>.</p>
-  <a class="btn" href="https://www.linkedin.com/in/lizandrofranco/">Linkedln</a>
-  <a class="btn" href="https://github.com/lizandrof273">Github</a>
+  <a class="btn" href="https://www.linkedin.com/in/lizandrofranco/" target="_blank">Linkedln</a>
+  <a class="btn" href="https://github.com/lizandrof273" target="_blank">Github</a>
 
  </div>
